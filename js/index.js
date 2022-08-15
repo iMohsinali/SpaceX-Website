cdb4637507830a3834a44ba3ef0e04ef23f94037
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-15 20:04:04 +0500

    counter

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 const btn = document.getElementById("menu-btn");
 const overlay = document.getElementById("overlay");
 const mobile = document.getElementById("mobile");
-
+const counter = document.querySelectorAll(".counter");
+let scrollStarted = false;
 btn.addEventListener("click", navToggle);
+document.addEventListener("scroll", scrollPage);
 
 function navToggle() {
   btn.classList.toggle("open");
@@ -10,8 +12,46 @@ function navToggle() {
   document.body.classList.toggle("stop-scrolling");
   mobile.classList.toggle("show-menu");
 }
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if (scrollPos > 100 && !scrollStarted) {
+    scrollStarted = true;
+    conteUp();
+  } else if (scrollPos < 100 && scrollStarted) {
+    reset();
+    scrollStarted = false;
+  }
+}
 
-console.log(mobile);
-console.log(overlay);
+scrollPage();
 
-console.log("this is mohsin");
+function conteUp() {
+  counter.forEach((counter) => {
+    counter.innerText = "0";
+    const updateCounter = () => {
+      //get target value
+      const target = +counter.getAttribute("data-target");
+      //   console.log(target);
+      //Get current counter value
+
+      const c = +counter.innerText;
+      //   console.log(c);
+
+      //increment
+      const increment = target / 100;
+      //if counter is less then the target ,add increment
+
+      if (c < target) {
+        counter.innerText = `${Math.ceil(c + increment)}`;
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCounter();
+  });
+}
+
+function reset() {
+  counter.forEach((counter) => (counter.innerHTML = 0));
+}
