3d465bb8ce74be29c464a5a12f63055ff7748bae
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-17 21:41:01 +0500

    Update App.js

--- SpaceX-React/src/App.js
+++ SpaceX-React/src/App.js
@@ -3,18 +3,28 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Mobile from "./components/Mobile";
 import Sections from "./components/Sections";
+import InnerSection from "./components/InnerSection";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const { isOpen } = useSelector((state) => state.menu);
-  console.log(isOpen);
+  const { isOpen, urlName } = useSelector((state) => state.menu);
+
+  // let match = useMatch();
+  // const nameUrl = window.location;
+
+  console.log(urlName);
   return (
-    <>
+    <Router>
       {isOpen && <div className="overlay"></div>}
       <Header />
       <Mobile />
-      <Sections />
+      <Routes>
+        <Route exact path="/" element={<Sections />} />
+        <Route path={urlName} element={<InnerSection />} />
+      </Routes>
+
       <Footer />
-    </>
+    </Router>
   );
 }
 
