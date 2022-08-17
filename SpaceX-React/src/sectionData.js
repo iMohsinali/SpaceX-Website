fd9856f4edfd03d6cd4d039f31170063b43d9c48
Mohsin ali <87361294+iMohsinali@users.noreply.github.com>
2022-08-17 21:41:54 +0500

    Update sectionData.js

--- SpaceX-React/src/sectionData.js
+++ SpaceX-React/src/sectionData.js
@@ -5,9 +5,10 @@ import sd from "./img/section-d.webp";
 import se from "./img/section-e.webp";
 import sf from "./img/section-f.webp";
 import falcon from "./img/falcon-9.webp";
+import falconHeavy from "./img/falcon-heavy.webp";
 import dragon from "./img/dragon.webp";
 
-const data = [
+export const data = [
   {
     id: 1,
     img: sa,
@@ -46,4 +47,54 @@ const data = [
   },
 ];
 
-export default data;
+export const inner = [
+  {
+    img: falcon,
+    h3: "FALCON 9",
+    p: "FIRSTORBIT CLASS ROCKET CAPCABLE ABEASD",
+    link: "/falcon9",
+  },
+  {
+    img: falconHeavy,
+    h3: "FALCON heavy",
+    p: "FIRSTORBIT CLASS ROCKET CAPCABLE ABEASD",
+    link: "/falcon-heavy",
+  },
+
+  {
+    img: dragon,
+    h3: "Dargon",
+    p: "FIRSTORBIT CLASS ROCKET CAPCABLE ABEASD",
+    link: "/dargon",
+  },
+  {
+    img: dragon,
+    h3: "Dream",
+    p: "Working",
+    link: "/dream",
+  },
+  {
+    img: dragon,
+    h3: "StarShip",
+    p: "Working",
+    link: "/starship",
+  },
+  {
+    img: dragon,
+    h3: "human spaceflight",
+    p: "Working",
+    link: "/human-spaceflight",
+  },
+  {
+    img: dragon,
+    h3: "Ride Share",
+    p: "Working",
+    link: "/rideshare",
+  },
+  {
+    img: dragon,
+    h3: "Shop",
+    p: "Working",
+    link: "/shop",
+  },
+];
